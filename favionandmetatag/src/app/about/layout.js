299a7ed2f973b377611ca9a5ea58b0a8c89b700e
Md-sakib-al-hasan <mdsakibalhasanprogrammer1@gmail.com>
2024-08-15 12:32:29 +0600

    generat_imge

--- favionandmetatag/src/app/about/layout.js
+++ favionandmetatag/src/app/about/layout.js
@@ -1,52 +1,33 @@
-// // app/layout.js or app/page.js
-
-// export const metadata = {
-//     metadataBase: new URL('http://localhost:3000'), // Use your root domain or localhost
-//     openGraph: {
-//         title: 'About Us',
-//         description: 'Learn more about our company.',
-//         url: 'http://localhost:3000/about', // Specific URL for the /about page
-//         images: [
-//             {
-//                 url: 'https://i.ibb.co/MPx3fbc/pexels-vince-2363808.jpg', // New image URL
-//                 width: 800,
-//                 height: 600,
-//                 alt: 'About Us Image'
-//             }
-//         ]
-//     },
-//     twitter: {
-//         card: 'summary_large_image',
-//         images: 'https://i.ibb.co/MPx3fbc/pexels-vince-2363808.jpg' // New image URL
-//     }
-// };
-
-
 // app/layout.js or app/page.js
 
 export const metadata = {
-    metadataBase: new URL('http://localhost:3000'), // Use your root domain or localhost
+    metadataBase: new URL(process.env.DOMEAN), // Use your root domain or localhost
     openGraph: {
         title: 'About Us',
         description: 'Learn more about our company.',
-        url: 'http://localhost:3000/about', // Specific URL for the /about page
+        url: `process.env.DOMEAN/about`, // Specific URL for the /about page
         images: [
             {
-                url: '/about/opengraph-image.png', // Relative path to the image
+                url: 'https://i.ibb.co/MPx3fbc/pexels-vince-2363808.jpg', // New image URL
                 width: 800,
                 height: 600,
-                alt: 'About Acme' // The alt text from opengraph-image.alt.txt
+                alt: 'sakib'
             }
         ]
     },
     twitter: {
         card: 'summary_large_image',
-        images: '/about/opengraph-image.png' // Relative path to the image
+        images: 'https://i.ibb.co/MPx3fbc/pexels-vince-2363808.jpg' // New image URL
     }
 };
 
 
+
+
+
+
 export default function RootLayout({ children }) {
+    console.log(process.env.DOMEAN)
     return (
         <div>
             {children}
---
+++ favionandmetatag/src/app/api/ingo/route.js
@@ -0,0 +1,8 @@
+
+
+export async function GET() {
+
+    const data = { naem: "sakib" }
+
+    return Response.json({ data })
+}
